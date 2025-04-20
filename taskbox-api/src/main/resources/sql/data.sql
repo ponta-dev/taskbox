3cb9394150481da64429c88f70f51d233c8900ec
ponta-dev <ponta.dev.matumoto@gmail.com>
2025-04-20 18:20:38 +0900

    H2DBをインメモリに変更

--- taskbox-api/src/main/resources/sql/data.sql
+++ taskbox-api/src/main/resources/sql/data.sql
@@ -1,3 +1,4 @@
 /* master_user */
+--delete from master_user;
 insert into master_user(user_id, user_name, user_password, description, age, mail_address, register_date, register_user, update_date, update_user)
 	values('sample_user', 'サンプル', 'password', '確認用の初期ユーザ', 20, 'sample@xxx.co.jp', '1990-01-01', 'admin', '1990-01-01', 'admin');
